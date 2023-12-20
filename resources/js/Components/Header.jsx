c4f025bd11ef7bb9cb83fd015e93f9ea2b1e1c44
edwinhati <edwinhati@outlook.com>
2023-12-21 03:34:10 +0800

    Update login and register links in Header component

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -185,14 +185,14 @@ export default function Header({ auth }) {
                                 href={route("login")}
                                 className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                             >
-                                Log in
+                                Masuk
                             </Link>
 
                             <Link
                                 href={route("register")}
                                 className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                             >
-                                Register
+                                Daftar
                             </Link>
                         </>
                     )}
@@ -294,14 +294,14 @@ export default function Header({ auth }) {
                                             href={route("login")}
                                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                         >
-                                            Log in
+                                            Masuk
                                         </Link>
 
                                         <Link
                                             href={route("register")}
                                             className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                         >
-                                            Register
+                                            Daftar
                                         </Link>
                                     </>
                                 )}
