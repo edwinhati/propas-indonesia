2506931116124c7bd1354bed5e12b1c64ae067c5
edwinhati <edwinhati@outlook.com>
2023-12-31 17:26:36 +0800

    Refactor Header Component

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,61 +1,7 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { Dialog, Popover } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "@inertiajs/react";
-import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-import {
-    ArrowPathIcon,
-    Bars3Icon,
-    ChartPieIcon,
-    CursorArrowRaysIcon,
-    FingerPrintIcon,
-    SquaresPlusIcon,
-    XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-    ChevronDownIcon,
-    PhoneIcon,
-    PlayCircleIcon,
-} from "@heroicons/react/20/solid";
-
-const products = [
-    {
-        name: "Analytics",
-        description: "Get a better understanding of your traffic",
-        href: "#",
-        icon: ChartPieIcon,
-    },
-    {
-        name: "Engagement",
-        description: "Speak directly to your customers",
-        href: "#",
-        icon: CursorArrowRaysIcon,
-    },
-    {
-        name: "Security",
-        description: "Your customers’ data will be safe and secure",
-        href: "#",
-        icon: FingerPrintIcon,
-    },
-    {
-        name: "Integrations",
-        description: "Connect with third-party tools",
-        href: "#",
-        icon: SquaresPlusIcon,
-    },
-    {
-        name: "Automations",
-        description: "Build strategic funnels that will convert",
-        href: "#",
-        icon: ArrowPathIcon,
-    },
-];
-const callsToAction = [
-    { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-    { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-}
 
 export default function Header({ auth }) {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -68,12 +14,8 @@ export default function Header({ auth }) {
             >
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            className="h-12 w-auto"
-                            src="/logo.jpeg"
-                            alt=""
-                        />
+                        <span className="sr-only">PROPAS INDONESIA</span>
+                        <img className="h-9 w-auto" src="/logo.jpeg" alt="" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
@@ -86,7 +28,26 @@ export default function Header({ auth }) {
                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                     </button>
                 </div>
-                
+                <Popover.Group className="hidden lg:flex lg:gap-x-12">
+                    <a
+                        href="#"
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                    >
+                        Beranda
+                    </a>
+                    <a
+                        href="#"
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                    >
+                        Organisasi
+                    </a>
+                    <a
+                        href="#news"
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                    >
+                        Berita
+                    </a>
+                </Popover.Group>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     {auth.user ? (
                         <Link
@@ -96,21 +57,20 @@ export default function Header({ auth }) {
                             Dashboard <span aria-hidden="true">&rarr;</span>
                         </Link>
                     ) : (
-                        <>
-                            <Link
-                                href={route("login")}
-                                className="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Masuk
-                            </Link>
-
+                        <div className="flex gap-2">
                             <Link
                                 href={route("register")}
-                                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                className="text-sm font-semibold leading-6 text-gray-900"
                             >
                                 Daftar
                             </Link>
-                        </>
+                            <Link
+                                href={route("login")}
+                                className="text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                Masuk
+                            </Link>
+                        </div>
                     )}
                 </div>
             </nav>
@@ -124,9 +84,9 @@ export default function Header({ auth }) {
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                            <span className="sr-only">PROPAS INDONESIA</span>
                             <img
-                                className="h-8 w-auto"
+                                className="h-9 w-auto"
                                 src="/logo.jpeg"
                                 alt=""
                             />
@@ -142,7 +102,26 @@ export default function Header({ auth }) {
                     </div>
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
-                            
+                            <div className="space-y-2 py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Beranda
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Organisasi
+                                </a>
+                                <a
+                                    href="#news"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Berita
+                                </a>
+                            </div>
                             <div className="py-6">
                                 {auth.user ? (
                                     <Link
@@ -153,21 +132,20 @@ export default function Header({ auth }) {
                                         <span aria-hidden="true">&rarr;</span>
                                     </Link>
                                 ) : (
-                                    <>
-                                        <Link
-                                            href={route("login")}
-                                            className="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                                        >
-                                            Masuk
-                                        </Link>
-
+                                    <div className="flex gap-2">
                                         <Link
                                             href={route("register")}
-                                            className="ms-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                            className="text-sm font-semibold leading-6 text-gray-900"
                                         >
                                             Daftar
                                         </Link>
-                                    </>
+                                        <Link
+                                            href={route("login")}
+                                            className="text-sm font-semibold leading-6 text-gray-900"
+                                        >
+                                            Masuk
+                                        </Link>
+                                    </div>
                                 )}
                             </div>
                         </div>
